739d48a9359e49570cd7b833969b0dfd28cfb4e0
AniceKV <kumaranish89457@gmail.com>
2026-08-28 20:55:16 +0530

    Cache local embedding model

--- base_rag.py
+++ base_rag.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 class LocalBGEEmbeddings(Embeddings):
     def __init__(self, model_name: str = "BAAI/bge-large-en-v1.5", device: str = "cpu"):
-        self.model = SentenceTransformer(model_name, device=device)
+        self.model = SentenceTransformer(model_name, device=device,cache_folder="./models")
         self.query_prefix = "Represent this sentence for searching relevant passages: "
         self.document_prefix = ""
 
